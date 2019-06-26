4b081dd4b4f9d9e6f37c1de5e5643365d28e11ca
Ali Gholami Rudi <ali@rudi.ir>
2019-06-27 01:27:44 +0430

    slow: clean up and merge the multi-path version

--- slow.c
+++ slow.c
@@ -1,137 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//a structure for locations
-struct node 
-{
-	float x, y, t;
-};
-//a structure for each path
-struct path
-{
-	struct node pathNodes[10000];
-};
-//a structure for each query
-struct query 
-{
-	float xL, yL, xR, yR, minT, maxT;  
+/* a trajectory node */
+struct node {
+	long x, y, t;
 };
 
-//function prototype
-void oneDimention_multiPath(struct path paths[100],struct query queries[100], int numberOfpaths, int numberOfNodes, int numberOfQueries);//for multi path
-void oneDimention_singlePath(struct path paths[100],struct query queries[100], int numberOfNodes, int numberOfQueries);//just for one path
+/* a trajectory */
+struct path {
+	struct node *nodes;
+	int n;
+};
 
+/* a query */
+struct query {
+	long llx, lly;		/* lower left corner */
+	long urx, ury;		/* upper right corner */
+	long minT, maxT;	/* minimum and maximum visit duration */
+};
 
-int main() 
+static int count_visits(struct path *path, struct query *query);
+
+int main(void)
 {
-	int i, j;//counters
-	int numberOfPaths, numberOfNodes, numberOfQueries;
-	int multiPath = 0;//a variable to specify we we have one path or more
-	
-//	printf("single path: 0 \nmulti path: 1\n");
-//	scanf("%d", &multiPath);	
-		
+	int numberOfPaths, numberOfQueries;
+	struct path *paths;
+	struct query *queries;
+	int i, j;
+
+	/* read input trajectories */
 	scanf("%d", &numberOfPaths);
-	struct path paths[numberOfPaths];
-	
-	//get locations for each path
-	for (i = 0; i < numberOfPaths; i++)
-	{
-		scanf("%d", &numberOfNodes);
-		for (j = 0; j < numberOfNodes; j++)
-		{
-			scanf("%f",&paths[i].pathNodes[j].t);						
-			scanf("%f",&paths[i].pathNodes[j].x);
-			scanf("%f",&paths[i].pathNodes[j].y);
+	paths = malloc(numberOfPaths * sizeof(paths[0]));
+	for (i = 0; i < numberOfPaths; i++) {
+		scanf("%d", &paths[i].n);
+		paths[i].nodes = malloc(paths[i].n * sizeof(paths[i].nodes[0]));
+		for (j = 0; j < paths[i].n; j++) {
+			scanf("%ld", &paths[i].nodes[j].t);
+			scanf("%ld", &paths[i].nodes[j].x);
+			scanf("%ld", &paths[i].nodes[j].y);
 		}
 	}
-	
+
+	/* read input queries */
 	scanf("%d", &numberOfQueries);
-	struct query queries[numberOfQueries];
-	
-	//get queries
-	for (i = 0; i < numberOfQueries; i++) 
-	{
-		scanf("%f", &queries[i].xL);
-		scanf("%f", &queries[i].yL);
-		scanf("%f", &queries[i].xR);
-		scanf("%f", &queries[i].yR);
-		scanf("%f", &queries[i].minT);
-		scanf("%f", &queries[i].maxT);
+	queries = malloc(numberOfQueries * sizeof(queries[0]));
+	for (i = 0; i < numberOfQueries; i++) {
+		scanf("%ld", &queries[i].llx);
+		scanf("%ld", &queries[i].lly);
+		scanf("%ld", &queries[i].urx);
+		scanf("%ld", &queries[i].ury);
+		scanf("%ld", &queries[i].minT);
+		scanf("%ld", &queries[i].maxT);
 	}
-	
-	if (multiPath == 0)//if we have one path
-		oneDimention_singlePath(paths ,queries, numberOfNodes, numberOfQueries);
-// 	else if (multiPath == 1)//if we have more than one path
-//	    oneDimention_multiPath(paths ,queries, numberOfPaths, numberOfNodes, numberOfQueries);
+
+	/* answer the queries */
+	for (i = 0; i < numberOfQueries; i++) {
+		int visits = 0;
+		for (j = 0; j < numberOfPaths; j++)
+			visits += count_visits(&paths[j], &queries[i]);
+		printf("%d\n", visits);
+	}
+
+	/* release allocated memory */
+	for (i = 0; i < numberOfPaths; i++)
+		free(paths[i].nodes);
+	free(paths);
+	free(queries);
+	return 0;
 }
 
-void oneDimention_singlePath(struct path paths[100],struct query queries[100], int numberOfNodes, int numberOfQueries) 
+static int count_visits(struct path *path, struct query *query)
 {
-	int i, j, z;
-	int count[100] = {0};
-	
-	for (j = 0; j < numberOfQueries; j++)//for each query
-	{
-		for (z = 0; z < numberOfNodes - 1; z++)//for each two node of a way  
-		{
-			if (paths[0].pathNodes[z].x < paths[0].pathNodes[z + 1].x)
-			{	
-				if (paths[0].pathNodes[z].x < queries[j].xR && paths[0].pathNodes[z + 1].x > queries[j].xL)				
-				{
-					count[j]++;
-				}
-			}
-			else if (paths[0].pathNodes[z].x > paths[0].pathNodes[z + 1].x)
-			{
-				if (paths[0].pathNodes[z].x > queries[j].xL && paths[0].pathNodes[z + 1].x < queries[j].xR)				
-				{
-					count[j]++;
-				}
-			}
+	int cnt = 0;		/* visit count */
+	int i;
+	for (i = 0; i < path->n - 1; i++) {
+		if (path->nodes[i].x < path->nodes[i + 1].x) {
+			if (path->nodes[i].x < query->urx && path->nodes[i + 1].x > query->llx)
+				cnt++;
+		} else if (path->nodes[i].x > path->nodes[i + 1].x) {
+			if (path->nodes[i].x > query->llx && path->nodes[i + 1].x < query->urx)
+				cnt++;
 		}
 	}
-	
-	//print answers
-	for (i = 0; i < numberOfQueries; i++) 
-	{
-		printf("%d\n", count[i]);
-	}
-}
-
-void oneDimention_multiPath(struct path paths[100],struct query queries[100], int numberOfpaths, int numberOfNodes, int numberOfQueries) 
-{
-	int i, j, z;//counters
-	int n = numberOfpaths;
-	int count[100] = {0};//store queries answers
-	
-	for (i = 0; i < n; i++)//for each path 
-	{
-		for (j = 0; j < numberOfQueries; j++)//for each query
-		{
-			for (z = 0; z < numberOfNodes - 1; z++)//for each two node of a way 
-			{
-				if (paths[i].pathNodes[z].x < paths[i].pathNodes[z + 1].x)
-				{	
-					if (paths[i].pathNodes[z].x < queries[j].xR && paths[i].pathNodes[z + 1].x > queries[j].xL)				
-					{
-						count[j]++;
-					}
-				}
-				else if (paths[i].pathNodes[z].x > paths[i].pathNodes[z + 1].x)
-				{
-					if (paths[i].pathNodes[z].x > queries[j].xL && paths[i].pathNodes[z + 1].x < queries[j].xR)				
-					{
-						count[j]++;
-					}
-				}
-			}
-		}
-	}
-	
-	//print answers
-	for (i = 0; i < numberOfQueries; i++) 
-	{
-		printf("%d\n", count[i]);
-	}
+	return cnt;
 }
