9ff45ed04aeb6cae614f2dfd84c38d3b93ad2272
Ali Gholami Rudi <ali@rudi.ir>
2019-07-19 21:54:18 +0430

    test1d: update some of the tests

--- test1d.c
+++ test1d.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* problem description test case */
 static void test00(void)
 {
 	printf("1\n");
@@ -16,67 +17,69 @@ static void test00(void)
 	printf("6 0 8 0 0 10\n");
 }
 
-//path will finish at the start of the second query
+/* path will finish at the start of the second query */
 void test01()
 {
 	printf("1\n");
 	printf("5\n");
 	printf("1 0 0\n");
-	printf("2 3 0\n");
-	printf("3 2 0\n");
-	printf("4 5 0\n");
-	printf("5 10 0\n");
+	printf("2 30 0\n");
+	printf("3 20 0\n");
+	printf("4 50 0\n");
+	printf("5 90 0\n");
 	printf("3\n");
-	printf("1 0 2 0 0 10\n");//asn: 3
-	printf("10 0 11 0 0 10\n");//ans: 0
-	printf("1 0 4 0 0 20\n");//ans: 3
+	printf("21 0 29 0 0 10\n");
+	printf("91 0 95 0 0 10\n");
+	printf("10 0 40 0 0 10\n");
 }
 
-//all path (instead one of them) will be in the last query
+//all path (instead of one of them) will be in the last query
 void test02()
 {
 	printf("1\n");
 	printf("5\n");
-	printf("1 1 0\n");
-	printf("2 2 0\n");
-	printf("3 3 0\n");
-	printf("4 6 0\n");
-	printf("5 1 0\n");
+	printf("1 10 0\n");
+	printf("2 20 0\n");
+	printf("3 30 0\n");
+	printf("4 60 0\n");
+	printf("5 10 0\n");
 	printf("2\n");
-	printf("-1 0 0 0 0 10\n");//ans: 0
-	printf("1 0 4 0 0 10\n");//ans: 4
+	printf("-1 0 0 0 0 10\n");
+	printf("15 0 40 0 0 10\n");
 }
-//path starts ,at start of one query(q1) | finishes in the middle of another query(q2)
+
 void test03()
 {
 	printf("1\n");
 	printf("5\n");
-	printf("1 2 0\n");
+	printf("1 0 0\n");
 	printf("2 5 0\n");
-	printf("3 7 0\n");
-	printf("4 2.8 0\n");
-	printf("5 8.8 0\n");
-	printf("2\n");
-	printf("2 0 3 0 0 10\n");//ans: 4
-	printf("8 0 9 0 0 15\n");// ans: 1
+	printf("3 10 0\n");
+	printf("4 5 0\n");
+	printf("5 0 0\n");
+	printf("4\n");
+	printf("3 0 7 0 0 10\n");
+	printf("2 0 4 0 0 10\n");
+	printf("6 0 8 0 0 10\n");
+	printf("6 0 12 0 0 10\n");
 }
 
-//start and end of the path is the same in query(q1 & q2)
+/* start and end of the path is the same in query(q1 & q2) */
 void test04()
 {
 	printf("1\n");
 	printf("7\n");
-	printf("1 1 0\n");
-	printf("2 2 0\n");
-	printf("3 3 0\n");
-	printf("4 4 0\n");
-	printf("5 5 0\n");
-	printf("6 3.5 0\n");
-	printf("7 1 0\n");
+	printf("1 10 0\n");
+	printf("2 20 0\n");
+	printf("3 30 0\n");
+	printf("4 40 0\n");
+	printf("5 50 0\n");
+	printf("6 35 0\n");
+	printf("7 10 0\n");
 	printf("3\n");
-	printf("0 0 1 0 0 10\n");//ans: 1
-	printf("1 0 2.5 0 0 10\n");// ans: 2
-	printf("2.5 0 4.5 0 0 10\n");// ans: 5
+	printf("11 0 15 0 0 10\n");
+	printf("12 0 25 0 0 10\n");
+	printf("25 0 45 0 0 10\n");
 }
 
 static void test05(void)
@@ -118,20 +121,15 @@ void test07()
 
 	printf("1\n");
 	printf("%d\n", n);
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; i++) {
 		printf("%d %d 0\n", i, c + i);
 		if ((c + i) % 5 == 0)
-		{
 			c += 10;
-		}
 	}
 
 	printf("%d\n", q);
 	for (int i = 0; i < q; i++)
-	{
 		printf("%d 0 %d 0 0 %d\n", i + l, i + r, n);
-	}
 }
 
 //time checking
@@ -145,8 +143,7 @@ void test08()
 
 	printf("1\n");
 	printf("%d\n", n);
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < n; i++) {
 		int c = rand() % (maxRange + 1 - minRange) + minRange;
 		s += i * c;
 		printf("%d %d 0\n", i, s);
@@ -157,16 +154,12 @@ void test08()
 	int l = 15;
 	int r = 20;
 	printf("%d\n", q);
-	for (int i = 0; i < q; i++)
-	{
+	for (int i = 0; i < q; i++) {
 		printf("%d 0 %d 0 0 %d\n", l, r, n);
-		if (i % 10 == 0)
-		{
+		if (i % 10 == 0) {
 			l -= 15;
 			r -= 15;
-		}
-		else
-		{
+		} else {
 			l += 10;
 			r += 10;
 		}
@@ -178,7 +171,7 @@ static void test09()
 	printf("100\n");
 	for (int i = 0; i < 100; i++) {
 		printf("99\n");
-		for(int j = 0; j<99 ; j++) {
+		for(int j = 0; j < 99; j++) {
 			printf("%d 0 0\n", i);
 			printf("%d 0 1\n", i+1);
 			printf("%d 0 0\n" ,i+2);
@@ -189,59 +182,65 @@ static void test09()
 	printf("0 0 1 0 0 100\n");
 }
 
-//tests by considering time limits
-//a test with two way
+/* tests with restrictions on visit duration */
+/* testing two trajectories */
 static void test10()
 {
 	printf("2\n");
 
-  	printf("4\n");
-  	printf("0 0 0\n");
-  	printf("5 4 0\n");
-  	printf("15 5 0\n");
-  	printf("25 -3 0\n");
+	printf("4\n");
+	printf("0 0 0\n");
+	printf("5 4 0\n");
+	printf("15 5 0\n");
+	printf("25 -3 0\n");
 
-  	printf("3\n");
-  	printf("5 -10 0\n");
-  	printf("15 10 0\n");
-  	printf("25 -10 0\n");
+	printf("3\n");
+	printf("5 -10 0\n");
+	printf("15 10 0\n");
+	printf("25 -10 0\n");
 
-  	printf("4\n");
-  	printf("0 0 4 0 0 10\n");
-  	printf("4 0 5 0 0 22\n");
-  	printf("-3 0 0 0 24 25\n");
-  	printf("-10 0 9 0 0 25\n");
+	printf("4\n");
+	printf("0 0 4 0 0 10\n");
+	printf("4 0 5 0 0 22\n");
+	printf("-3 0 0 0 24 25\n");
+	printf("-10 0 9 0 0 25\n");
 }
 
 //in middle of time and in middle of the path
 static void test11()
 {
 	printf("1\n");
-  	printf("4\n");
-  	printf("1 4 0\n");
-  	printf("5 -7 0\n");
-  	printf("7 11 0\n");
+	printf("3\n");
+	printf("0 0 0\n");
+	printf("10 10 0\n");
+	printf("20 20 0\n");
 
-  	printf("2\n");
-  	printf("0 0 2 0 4 6\n");
-  	printf("4 0 5 0 2 7\n");
+	printf("8\n");
+	printf("5 0 6 0 2 10\n");
+	printf("5 0 7 0 2 10\n");
+	printf("5 0 8 0 2 10\n");
+	printf("5 0 15 0 2 10\n");
+	printf("5 0 6 0 0 2\n");
+	printf("5 0 7 0 0 2\n");
+	printf("5 0 8 0 0 2\n");
+	printf("5 0 15 0 0 2\n");
 }
 
 //in middle of time and at the edge of the path
 static void test12()
 {
 	printf("1\n");
-  	printf("5\n");
-  	printf("0 0 0\n");
-  	printf("12 10 0\n");
-  	printf("13 6 0\n");
-  	printf("14 5 0\n");
-  	printf("15 0 0\n");
+	printf("5\n");
+	printf("0 0 0\n");
+	printf("12 100 0\n");
+	printf("13 60 0\n");
+	printf("14 50 0\n");
+	printf("15 0 0\n");
 
-  	printf("3\n");
-  	printf("0 0 10 0 0 14\n");//ans: 1
-  	printf("5 0 6 0 13 15\n");//ans: 1
-  	printf("1 0 2 0 4 14\n");//ans: 0
+	printf("3\n");
+	printf("1 0 99 0 0 14\n");//ans: 1
+	printf("49 61 6 0 13 15\n");//ans: 1
+	printf("10 0 20 0 4 14\n");//ans: 0
 }
 
 int main(int argc, char *argv[])
